d594d421a962aecf8df491a127f722177f44bf00
Samuel Mintah <samiblaq8@gmail.com>
2023-04-12 12:19:06 +0000

    folder

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,8 +4,21 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  mode: "jit",
   theme: {
-    extend: {},
+    fontFamily: {
+      Roboto: ["Roboto", "sans-serif"],
+      Poppins: ['Poppins', "sans-serif"],
+    },
+    extend: {
+      screens: {
+        "1000px": "1050px",
+        "1100px": "1110px",
+        "800px": "800px",
+        "1300px": "1300px",
+        "400px":"400px"
+      },
+    },
   },
   plugins: [],
-}
+};
