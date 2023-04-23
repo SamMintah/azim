897ab08518bf8672be947f5a19d13304321d25ff
Samuel Mintah <samiblaq8@gmail.com>
2023-04-23 08:40:25 +0000

    coupon

--- server/controller/couponCode.js
+++ server/controller/couponCode.js
@@ -1,23 +1,23 @@
 const Shop = require("../model/shop");
 const ErrorHandler = require("../utils/ErrorHandler");
-const CoupounCode = require("../model/coupounCode");
+const couponCode = require("../model/couponCode");
 
 // create coupoun code
-const createCoupoun = async (req, res, next) => {
+const createCoupon = async (req, res, next) => {
     try {
-        const isCoupounCodeExists = await CoupounCode.find({
+        const iscouponCodeExists = await couponCode.find({
             name: req.body.name,
         });
 
-        if (isCoupounCodeExists.length !== 0) {
+        if (iscouponCodeExists.length !== 0) {
             return next(new ErrorHandler("Coupoun code already exists!", 400));
         }
 
-        const coupounCode = await CoupounCode.create(req.body);
+        const couponCode = await couponCode.create(req.body);
 
         res.status(201).json({
             success: true,
-            coupounCode,
+            couponCode,
         });
     } catch (error) {
         return next(new ErrorHandler(error, 400));
@@ -25,9 +25,9 @@ const createCoupoun = async (req, res, next) => {
 };
 
 // get all coupons of a shop
-const getCoupouns = async (req, res, next) => {
+const getCoupons = async (req, res, next) => {
     try {
-        const couponCodes = await CoupounCode.find({ shopId: req.seller.id });
+        const couponCodes = await couponCode.find({ shopId: req.seller.id });
         res.status(201).json({
             success: true,
             couponCodes,
@@ -38,9 +38,9 @@ const getCoupouns = async (req, res, next) => {
 };
 
 // delete coupoun code of a shop
-const deleteCoupoun = async (req, res, next) => {
+const deleteCoupon = async (req, res, next) => {
     try {
-      const couponCode = await CoupounCode.findByIdAndDelete(req.params.id);
+      const couponCode = await couponCode.findByIdAndDelete(req.params.id);
 
       if (!couponCode) {
         return next(new ErrorHandler("Coupon code dosen't exists!", 400));
@@ -56,7 +56,7 @@ const deleteCoupoun = async (req, res, next) => {
   
 
 module.exports = {
-    createCoupoun,
-    getCoupouns,
-    deleteCoupoun,
+    createCoupon,
+    getCoupons,
+    deleteCoupon,
 };
